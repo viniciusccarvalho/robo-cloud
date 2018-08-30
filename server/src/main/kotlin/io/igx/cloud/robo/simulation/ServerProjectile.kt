28e286c8207c7cd22b163614e17ebc53ceb43209
Vinicius Carvalho <viniciusccarvalho@gmail.com>
2018-08-29 21:14:39 -0400

    Added cool parallax background

--- server/src/main/kotlin/io/igx/cloud/robo/simulation/ServerProjectile.kt
+++ server/src/main/kotlin/io/igx/cloud/robo/simulation/ServerProjectile.kt
@@ -2,18 +2,18 @@ package io.igx.cloud.robo.simulation
 
 import io.igx.cloud.robo.Movable
 import io.igx.cloud.robo.proto.Projectile
-import org.apache.commons.math3.geometry.euclidean.twod.Vector2D
+import org.jbox2d.common.Vec2
 
 /**
  * @author vinicius
  * Represents a "live" projectile, trajectory is updated on every frame
  */
-class ServerProjectile(var bearing: Double = 0.0, var center: Vector2D = Vector2D(0.0, 0.0)) : Movable {
+class ServerProjectile(var bearing: Float = 0.0f, var center: Vec2 = Vec2(0.0f, 0.0f)) : Movable {
 
-    val speed = 30.0/1_000 // pixels per second
+    val speed = 30.0f/1_000 // pixels per second
 
     override fun updateCoordinates(delta: Long) {
-        center = center.moveTo(Math.toRadians(bearing), speed*delta)
+        center = center.moveTo(bearing, speed*delta)
     }
 
     fun getState() : Projectile {
